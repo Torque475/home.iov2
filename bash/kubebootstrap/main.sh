30dc772e6b67ae3d32b48bfb184bf66a1105f1e5
Torque <Torque475@github>
2024-08-28 18:33:33 -0500

    got k3sup join with a node taint functional. Requires kube-vip prereq

--- bash/kubebootstrap/main.sh
+++ bash/kubebootstrap/main.sh
@@ -15,12 +15,18 @@
 # Preset install options 
 rancher_hosts="rancher_hosts.json"
 homelab_hosts="kube_hosts.json"
+kube_vip="10.1.5.20"
+gpu_hosts="gpu_node.json"
+gpu_node_ip="10.1.5.50"
 # Single master node, HA requires 3
+# By default all nodes, including masters will deploy workloads, no reason to not have HA
 rancher_servers=1
-homelab_servers=1
+homelab_servers=3
 # k config context names
 rancher_context_name="rancher"
 homelab_context_name="homelab"
+
+
 
 # host file outside of my presets can be entered as first argument
 unique_hosts_file=$1
@@ -33,7 +39,9 @@ config_path="~/.kube/config"
 ssh_key_path="~/.ssh/torque"
 user="torque"
 
+k3s_extra_args="--disable servicelb"
 k3s_version="v1.28.8+k3s1"
+gpu_extra_args='--node-taint node_type=gpu:NoSchedule' # Unneeded as there's already options to add that.
 
 # Rancher Server variables
 cert_manager_version=v1.14.4
@@ -69,6 +77,7 @@ function k3s_install {
     local context_name=$3
     local servers=$4
     local user=$5
+    local extra_args=$6
     #Create the k3sup install script
     echo "Creating execution plan"
     k3sup plan \
@@ -77,10 +86,13 @@ function k3s_install {
         --context $context_name \
         --servers $servers \
         --user $user \
+        --agent-k3s-extra-args "$extra_args" \
+        --server-k3s-extra-args "$extra_args" \
         > $hosts"_bootstrap.sh"
     
     # Add k3s-version flag to plan (since it's not an option)
-    sed -i "s/--user $user/& --k3s-version $k3s_version/g" $hosts"_bootstrap.sh"
+    # UNTESTED - adding kube_vip to the TLS san as an addition node.
+    sed -i "s/--user $user/& --k3s-version $k3s_version --tls-san $kube_vip/g" $hosts"_bootstrap.sh"
 
     #Run it to actually create the cluster
     chmod +x $hosts"_bootstrap.sh"
@@ -89,12 +101,30 @@ function k3s_install {
     echo "Removing bootstrap script"
     rm $hosts"_bootstrap.sh" 
 
-    #Merging kubeconfigs to primary
+    Merging kubeconfigs to primary
     export KUBECONFIG=~/.kube/config:$PWD/kubeconfig
     kubectl config view --merge --flatten > ~/.kube/merged_config
     mv ~/.kube/merged_config ~/.kube/config
     chmod 600 ~/.kube/config
     rm kubeconfig
+}
+
+function gpu_node_install {
+  #Variable passing
+  local ssh_key_path=$1
+  local node_ip=$2
+  local server_ip=$3
+  local user=$4
+  local gpu_extra_args=$5
+  local k3s_version=$6
+  
+  # This uses k3sup join rather than k3sup plan. Requires different syntax and information
+
+  # Installs and joins gpu node to cluster. 
+  # Requires Kube-VIP to already be deployed with the master nodes with TLS fixed
+
+  k3sup join --ip $node_ip --server-ip $server_ip --user $user --k3s-extra-args "$gpu_extra_args" --ssh-key $ssh_key_path --k3s-version $k3s_version
+
 }
 
 function rancher_install {
@@ -150,6 +180,7 @@ function show_menu {
     echo "3) Create homelab cluster"
     echo "4) User defined cluster - Requires appropriate host.json and context name"
     echo "5) Install Rancher server on rancher cluster"
+    echo "6) Add gpu node as k3s_agent
     echo "9) Exit script"
 
     read -p "Enter choice [1-5, 9]: " choice
@@ -160,18 +191,22 @@ function show_menu {
           ;;
         2)
           #hosts = $1, ssh_path = $2, context_name = $3, server count = $4, user = $5
-          k3s_install $rancher_hosts $ssh_key_path $rancher_context_name $rancher_servers $user
+          k3s_install $rancher_hosts $ssh_key_path $rancher_context_name $rancher_servers $user $k3s_extra_args
           ;;
         3)
           #hosts = $1, ssh_path = $2, context_name = $3, server count = $4 user = $5
-          k3s_install $homelab_hosts $ssh_key_path $homelab_context_name $homelab_servers $user
+          k3s_install $homelab_hosts $ssh_key_path $homelab_context_name $homelab_servers $user $k3s_extra_args
           ;;
         4) 
-          #hosts = $1, ssh_path = $2, context_name = $3, server count = $4 user = $5
-          k3s_install $unique_hosts_file $ssh_key_path $unique_context_name $unique_server_count $unique_user
+          #hosts = $1, ssh_path = $2, context_name = $3, server count = $4 user = $5 extra_args = $6
+          k3s_install $unique_hosts_file $ssh_key_path $unique_context_name $unique_server_count $unique_user $k3s_extra_args
           ;;
         5)
           rancher_install
+          ;;
+        6)
+          # ssh_key_path=$1 node_ip=$2 server_ip=$3 user=$4 gpu_extra_args=$5
+          gpu_node_install $ssh_key_path $gpu_node_ip $kube_vip $user $gpu_extra_args $k3s_version
           ;;
         9)
           echo "Exiting..."
